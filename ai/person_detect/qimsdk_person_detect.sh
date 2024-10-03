d53b1b8082d331ad3231e7079d152bc4b83cc45f
Andy Tran <quic_andytran@quicinc.com>
2024-10-03 13:33:43 -0700

    Person Detect: Adding Container Build Files
    - shell script with gst-launch command for
    Person Detect microservice
    - providing dockerfile to build container image for
    Person Detect ai pipeline based on QIMSDK
    
    Change-Id: Ia75d3e1ed46ec7f150f4784184b629984813df3b

---
+++ ai/person_detect/qimsdk_person_detect.sh
@@ -0,0 +1,14 @@
+# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e \
+qtimlvconverter name=stage_01_preproc mode=image-batch-non-cumulative \
+qtimlqnn name=stage_01_inference model=/opt/data/libqnn2.24_PD.so backend=/usr/lib/libQnnHtp.so \
+qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results=10 module=qpd labels=/opt/data/qpd.labels \
+rtspsrc location=$1 ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=5 output-io-mode=5 ! queue ! tee name=t_split_1 \
+t_split_1. ! queue ! metamux_1. \
+t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_4 \
+t_split_4. ! queue ! waylandsink async=false name=display sync=false fullscreen=true \
+t_split_4. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel="Detection::YoloV8::RZ::0" host="172.17.0.1" port=6379 \
+t_split_4. ! queue ! v4l2h264enc name=encoder capture-io-mode=5 output-io-mode=5 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=8900
