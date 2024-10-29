a9b7157f3299dc1c2631ab79a80561ded02e6331
Amrutha Sai Gattu <quic_agattu@quicinc.com>
2024-10-28 18:37:34 -0700

    ai: update qimsdk microservices with tflite models
    - update ppe, rz, person-detect microservices with tflite model
    
    Change-Id: I5141a1709fe22bc377d6f14aef382f7b80ca5796

--- ai/rz/qimsdk_restricted_zone.sh
+++ ai/rz/qimsdk_restricted_zone.sh
@@ -2,10 +2,10 @@
 # Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e -v --gst-debug=2 \
+export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e --gst-debug=2 \
 qtimlvconverter name=stage_01_preproc mode=image-batch-non-cumulative \
-qtimlqnn name=stage_01_inference model=/opt/data/libqnn2.24_PD.so backend=/usr/lib/libQnnHtp.so \
-qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results=10 module=qpd labels=/opt/data/qpd.labels  \
+qtimltflite name=stage_01_inference delegate=external external-delegate-path=libQnnTFLiteDelegate.so external-delegate-options="QNNExternalDelegate,backend_type=htp;" model=/opt/data/foot_track_net_quantized.tflite \
+qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results=10 module=qpd constants="qpd,q-offsets=<0.0,25.0,114.0,0.0>,q-scales=<0.0035636962857097387,1.2998489141464233,2.1744511127471924,0.00390625>;" labels=/opt/data/qpd.labels \
 rtspsrc location=$1 ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=5 output-io-mode=5 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
