4f85bad42a4dde35ccae59e9e33e580efc280613
Konstantin Motov <quic_kmotov@quicinc.com>
2024-11-14 11:42:46 +0200

    ai: Update access control workload script
    Change-Id: Ibb0707c44a8618e3fb7bd3775e951df97e3285b8

--- ai/access-control/qimsdk_access_control.sh
+++ ai/access-control/qimsdk_access_control.sh
@@ -12,7 +12,7 @@ qtimlvpose name=stage_02_postproc threshold=51.0 results=6 module=lite-3dmm labe
 qtimlvconverter name=stage_03_preproc mode=roi-batch-cumulative \
 qtimlqnn name=stage_03_inference model=/opt/data/libqnn2.24_FR.so backend=/usr/lib/libQnnHtp.so \
 qtimlvclassification name=stage_03_postproc threshold=60.0 results=6 module=qfr labels=/opt/data/qfr.labels \
-rtspsrc location=$INPUT_URL ! queue ! rtpptdemux ! rtph264depay ! h264parse ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=5 output-io-mode=5 ! queue ! tee name=t_split_1 \
+rtspsrc location=$INPUT_URL ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=5 output-io-mode=5 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
 qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! queue ! tee name=t_split_2 \
@@ -26,7 +26,7 @@ t_split_4. ! queue ! waylandsink name=display sync=false async=false fullscreen=
 t_split_4. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false  channel=$REDIS_DETECTION_CHANNEL host="172.17.0.1" port=6379 \
 t_split_4. ! queue ! v4l2h264enc name=encoder capture-io-mode=5 output-io-mode=5 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=8900
 
-then
+else
 
 export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e \
 qtimlvconverter name=stage_01_preproc mode=image-batch-non-cumulative \
@@ -38,7 +38,7 @@ qtimlvpose name=stage_02_postproc threshold=51.0 results=6 module=lite-3dmm labe
 qtimlvconverter name=stage_03_preproc mode=roi-batch-cumulative \
 qtimlqnn name=stage_03_inference model=/opt/data/libqnn2.24_FR.so backend=/usr/lib/libQnnHtp.so \
 qtimlvclassification name=stage_03_postproc threshold=60.0 results=6 module=qfr labels=/opt/data/qfr.labels \
-filesrc location=$INPUT_URL ! qtdemux ! h264parse config-interval=1 ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=5 output-io-mode=5 ! queue ! tee name=t_split_1 \
+filesrc location=$INPUT_URL ! qtdemux ! h264parse config-interval=1 ! v4l2h264dec capture-io-mode=5 output-io-mode=5 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
 qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! queue ! tee name=t_split_2 \
