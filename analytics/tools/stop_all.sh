ebac1fe3bf7f3c7dedc3ca479c58cacd7a1e7019
Jayson Smith <quic_jrsmith@quicinc.com>
2024-12-18 13:57:04 -0800

    analytics: Remove tools/ folder
    With move to use-cases/ folders with docker-compose, these scripts
    are outdated and no longer needed.
    
    Change-Id: I1a10a9a33c2da3dddd519306f9b1a4e9ad492dc8

--- analytics/tools/stop_all.sh
+++
@@ -1,3 +0,0 @@
-# Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
-# SPDX-License-Identifier: BSD-3-Clause-Clear
-docker rm -f mariadb redis people-analytics-server region-analytics-server people-analytics-api region-analytics-api nginx camera-api
