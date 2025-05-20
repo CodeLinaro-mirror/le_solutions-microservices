c99858ca26e18e0edced1930ef83b85666ee3207
Amrutha Sai Gattu <quic_agattu@quicinc.com>
2025-05-19 22:50:31 -0700

    solutions-microservices: update the shell scripts/compose files
    - add support for on-device-camera to use ISP camera
    - support both mp4 and ts files playback
    - use multifilesrc instead of filesrc to use ts files for continuous playback
    - make redis out port as a parameter
    
    Change-Id: I2a3b91e8a6fc658ee5af171e35b0cbfcda32247d

--- ai/person-detect/qimsdk_person_detect.sh
+++ ai/person-detect/qimsdk_person_detect.sh
@@ -9,23 +9,43 @@ qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results
 rtspsrc location=${INPUT_URL} ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
-qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_4 \
-t_split_4. ! queue ! waylandsink async=false sync=false fullscreen=true \
-t_split_4. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=6379 \
-t_split_4. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_2 \
+t_split_2. ! queue ! waylandsink async=false sync=false fullscreen=true \
+t_split_2. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=${REDIS_OUT_PORT} \
+t_split_2. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
 
-else
+elif [[ $INPUT_TYPE == "on-device-camera" ]]; then
 
 export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e \
 qtimlvconverter name=stage_01_preproc mode=image-batch-non-cumulative \
 qtimltflite name=stage_01_inference delegate=external external-delegate-path=libQnnTFLiteDelegate.so external-delegate-options="QNNExternalDelegate,backend_type=htp;" model=${MODEL_PATH_PERSON} \
 qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results=10 module=qpd constants="qpd,q-offsets=${MODEL_OFFSETS_PERSON},q-scales=${MODEL_SCALES_PERSON};" labels=${LABELS_PATH_PERSON} \
-filesrc location=${INPUT_URL} ! qtdemux ! h264parse config-interval=1 ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
+qtiqmmfsrc name=camsrc camera=${INPUT_URL} ! video/x-raw,format=NV12,width=1920,height=1080,framerate=30/1,interlace-mode=progressive,colorimetry=bt601 ! identity sync=true ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
-qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_4 \
-t_split_4. ! queue ! waylandsink async=false sync=true fullscreen=true \
-t_split_4. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=6379 \
-t_split_4. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_2 \
+t_split_2. ! queue ! waylandsink async=false sync=true fullscreen=true \
+t_split_2. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=${REDIS_OUT_PORT} \
+t_split_2. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
+
+else
+
+demuxer=qtdemux
+
+if [[ "${INPUT_URL##*.}" == "ts" ]]; then
+demuxer=tsdemux
+fi
+
+export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e \
+qtimlvconverter name=stage_01_preproc mode=image-batch-non-cumulative \
+qtimltflite name=stage_01_inference delegate=external external-delegate-path=libQnnTFLiteDelegate.so external-delegate-options="QNNExternalDelegate,backend_type=htp;" model=${MODEL_PATH_PERSON} \
+qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results=10 module=qpd constants="qpd,q-offsets=${MODEL_OFFSETS_PERSON},q-scales=${MODEL_SCALES_PERSON};" labels=${LABELS_PATH_PERSON} \
+multifilesrc location=${INPUT_URL} ! ${demuxer} ! h264parse config-interval=1 ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
+t_split_1. ! queue ! metamux_1. \
+t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_2 \
+t_split_2. ! queue ! waylandsink async=false sync=true fullscreen=true \
+t_split_2. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=${REDIS_OUT_PORT} \
+t_split_2. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
 
 fi
