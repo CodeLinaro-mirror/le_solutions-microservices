50ec3bdb8eeda9532f701f44416d03964b76c579
Stilyan Uzunov <suzunov@qti.qualcomm.com>
2025-06-20 09:52:41 +0300

    solutions-microservices: Add support for .mp4 input
    - Add support for .mp4 files alongside .ts files as input.
    - Change source plugin between multifilesrc for .ts files and filesrc
    for .mp4 containers /not supported by multifilesrc/
    
    Change-Id: I949b051ad537188ba0b34c6ec6399cb21ed9f31e

--- ai/ppe/qimsdk_ppe.sh
+++ ai/ppe/qimsdk_ppe.sh
@@ -44,9 +44,11 @@ t_split_3. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-i
 else
 
 demuxer=qtdemux
+source_sequence="filesrc location=${INPUT_URL} ! ${demuxer}"
 
 if [[ "${INPUT_URL##*.}" == "ts" ]]; then
-demuxer=tsdemux
+    demuxer=tsdemux
+    source_sequence="multifilesrc location=${INPUT_URL} ! ${demuxer}"
 fi
 
 export XDG_RUNTIME_DIR=/dev/socket/weston && export WAYLAND_DISPLAY=wayland-1 && ulimit -n 4096 && gst-launch-1.0 -e \
@@ -56,7 +58,7 @@ qtimlvdetection name=stage_01_postproc threshold=90.0 stabilization=true results
 qtimlvconverter name=stage_02_preproc mode=roi-batch-cumulative \
 qtimltflite name=stage_02_inference delegate=external external-delegate-path=libQnnTFLiteDelegate.so external-delegate-options="QNNExternalDelegate,backend_type=htp;" model=${MODEL_PATH_PPE} \
 qtimlvdetection name=stage_02_postproc threshold=75.0 stabilization=true results=10 module=yolov5 constants="YoloV5,q-offsets=${MODEL_OFFSETS_PPE},q-scales=${MODEL_SCALES_PPE};" labels=${LABELS_PATH_PPE} \
-multifilesrc location=${INPUT_URL} ! ${demuxer} ! h264parse config-interval=1 ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
+${source_sequence} ! h264parse config-interval=1 ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
 qtimetamux name=metamux_1 ! queue ! tee name=t_split_2 \
