d90fdb2c79969de581208664141aa67ddbac5c51
Ishu Raj <iraj@qti.qualcomm.com>
2025-07-21 19:57:47 +0530

    solutions-microservices: Enable wizard script in Ubuntu
    Change-Id: I064896152c75ca6bd540a9d49bc115a76ed92790

---
+++ microservices/qimsdk/docker/scripts/qcs6490_ubun_docker_run.sh
@@ -0,0 +1,4 @@
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+# SPDX-License-Identifier: BSD-3-Clause-Clear
+
+docker run -it -d --net host --device qualcomm.com/device=cdi-hw-acc -e XDG_RUNTIME_DIR=/run/user/1000 -e WAYLAND_DISPLAY=wayland-1 -e GST_DEBUG_NO_COLOR=1 -e GST_DEBUG=2 -e GST_PLUGIN_SCANNER="/usr/lib/aarch64-linux-gnu/gstreamer1.0/gstreamer-1.0/gst-plugin-scanner" -h qimsdk --user ubuntu --name qimsdk qimsdk
