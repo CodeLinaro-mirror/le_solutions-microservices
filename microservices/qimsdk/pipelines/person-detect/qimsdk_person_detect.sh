85faa8094a2601a65154e9ee85728fb7b15ef208
Ishu Raj <iraj@qti.qualcomm.com>
2026-02-27 18:33:44 +0530

    solutions-microservices: Remove engine property from voverlay
    'engine' property has been removed from the plugin qtivoverlay as it was
    redundant. qtivoverlay operates with GLES backend by default.
    
    Change-Id: I25705d8fc2d4ef2c56c2d0cbf75d36adb9d8e01a
    Signed-off-by: iraj <iraj@qti.qualcomm.com>
    (cherry picked from commit e3c9fda416c749951c90906cc2223baa248d1104)

--- microservices/qimsdk/pipelines/person-detect/qimsdk_person_detect.sh
+++ microservices/qimsdk/pipelines/person-detect/qimsdk_person_detect.sh
@@ -9,7 +9,7 @@ qtimlpostprocess name=stage_01_postproc results=10 module=qpd labels=${LABELS_PA
 rtspsrc location=${INPUT_URL} ! queue ! rtpptdemux ! rtph264depay ! h264parse ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
-qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_2 \
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay ! queue ! tee name=t_split_2 \
 t_split_2. ! queue ! waylandsink async=false sync=false fullscreen=true \
 t_split_2. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=${REDIS_OUT_PORT} \
 t_split_2. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
@@ -23,7 +23,7 @@ qtimlpostprocess name=stage_01_postproc results=10 module=qpd labels=${LABELS_PA
 qtiqmmfsrc name=camsrc camera=${INPUT_URL} ! video/x-raw,format=NV12,width=1280,height=720,framerate=30/1,interlace-mode=progressive,colorimetry=bt601 ! identity sync=true ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
-qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_2 \
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay ! queue ! tee name=t_split_2 \
 t_split_2. ! queue ! waylandsink async=false sync=true fullscreen=true \
 t_split_2. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=${REDIS_OUT_PORT} \
 t_split_2. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
@@ -45,7 +45,7 @@ qtimlpostprocess name=stage_01_postproc results=10 module=qpd labels=${LABELS_PA
 ${source_sequence} ! h264parse config-interval=1 ! v4l2h264dec capture-io-mode=4 output-io-mode=4 ! video/x-raw,format=NV12 ! queue ! tee name=t_split_1 \
 t_split_1. ! queue ! metamux_1. \
 t_split_1. ! queue ! stage_01_preproc. stage_01_preproc. ! queue ! stage_01_inference. stage_01_inference. ! queue ! stage_01_postproc. stage_01_postproc. ! text/x-raw ! queue ! metamux_1. \
-qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay engine=gles ! queue ! tee name=t_split_2 \
+qtimetamux name=metamux_1 ! queue ! qtiobjtracker ! qtivoverlay ! queue ! tee name=t_split_2 \
 t_split_2. ! queue ! waylandsink async=false sync=true fullscreen=true \
 t_split_2. ! queue ! qtimlmetaparser module=json ! qtiredissink sync=false async=false channel=${REDIS_DETECTION_CHANNEL} host="172.17.0.1" port=${REDIS_OUT_PORT} \
 t_split_2. ! queue ! identity sync=true ! v4l2h264enc capture-io-mode=4 output-io-mode=4 ! queue ! h264parse config-interval=1 ! queue ! qtirtspbin address=0.0.0.0 port=${OUT_PORT}
